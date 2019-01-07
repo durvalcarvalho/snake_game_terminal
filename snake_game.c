85d1d57e734c6434243b21ba496e7226b0496fe8
Durval Carvalho <durvalcsouza@outlook.com>
2019-01-07 17:02:01 -0200

    Using ncurses

--- snake_game.c
+++ snake_game.c
@@ -1,184 +1,257 @@
 #include <stdio.h>
+#include <ncurses.h>
+#include <string.h>
 #include <stdlib.h>
-#include <unistd.h> // using sleep()
 
-typedef struct _snake_piece
+// MACROS
+#define WIDTH 50
+#define HEIGHT 20
+
+
+// STRUCTURES
+typedef struct _snake_body
 {
 	int pos_x;
 	int pos_y;
-	char piece; // '-', '|', '>','<','^','V'
-
-} Snake_Piece;
+	char body; // '>', '<', '^', 'V'
+	
+} Snake_Body;
 
 typedef struct _snake
 {
-	int horiz; // +1 - moving right      -1 -moving left
-	int verti; // +1 - moving up      -1 -moving down
+	char direction; // D, U, R, L (Down, Up, Right, Left)
 	int size;
-
-	Snake_Piece* Pieces;
-	// struct with all snake parts and related chars
-
+	Snake_Body* Body;
 } Snake;
 
-char** generate(int, int);
-void print_table(char**, Snake*,int, int);
-int eat_itself(Snake*);
-int playing(char**,Snake*, int, int);
-void move_snake(Snake*);
-void add_piece(Snake*, int, int);
+
+// FUNCTIONS
+void print_table(WINDOW*, char**, Snake*);
+char** create_table(int, int, char);
+Snake create_snake(int, int);
+void insert_snake(char**, Snake*);
+bool isGameOver(Snake*);
+void movement_snake(Snake*);
 
 int main()
 {
-	int altura=30, largura=30;
-	// puts("Digite a altura e largura do tabuleiro: ");
-	// scanf("%d %d", &altura, &largura);
+	initscr();
+	clear();
+	noecho();
+	// cbreak(); // Line buffering disabled, pass on everything
+	curs_set(0); // disable blink cursor
 
-	char** tabuleiro = generate(altura, largura);
+	int startx, starty = 0;
+	getmaxyx(stdscr, starty, startx);
 
-	Snake snake;
-	snake.size = 0;
-	snake.horiz = 1; // moving to the right
-	snake.verti = 0; // not movint vertical
+	// Creating a main window and centering
+	WINDOW* main_win = newwin(HEIGHT, WIDTH, starty/5, (startx-WIDTH)/2);
 
-	// head and tail
-	add_piece(&snake, altura, largura);
+	// geting arrow keys
+	keypad(main_win, TRUE);
 
-	while( playing(tabuleiro, &snake, altura, largura) )
+	char msg[] = "Use as setas ou 'ASDW' para jogar";
+	mvprintw(1, (startx-strlen(msg))/2, "%s", msg);
+	refresh();
+
+	char** table;
+	table = create_table(HEIGHT, WIDTH, '.');
+
+	// init snake
+	Snake snake = create_snake(HEIGHT, WIDTH);
+
+	print_table(main_win, table, &snake);
+
+	int c;
+	while(TRUE)
 	{
-		move_snake(&snake);
+		if(isGameOver(&snake)) break;
 
-		print_table(tabuleiro, &snake, altura, largura);
+		c = wgetch(main_win);
+
+		halfdelay(1);
+
+		switch(c)
+		{
+			case 83: // 'S'
+			case 115: // 's'
+			case KEY_DOWN:
+				if(snake.direction != 'U')
+					snake.direction = 'D';
+				break;
+
+			case 87: // 'W'
+			case 119: // 'w'
+			case KEY_UP:
+				if(snake.direction != 'D')
+					snake.direction = 'U';
+				break;
+
+			case 68: // 'D'
+			case 100: // 'd'
+			case KEY_RIGHT:
+				if(snake.direction != 'L')
+					snake.direction = 'R';
+				break;
+
+			case 65: // 'A'
+			case 97: // 'a'
+			case KEY_LEFT:
+				if(snake.direction != 'R')
+					snake.direction = 'L';
+				break;
+
+		}
+
+		movement_snake(&snake);
+
+		print_table(main_win, table, &snake);
+
+		refresh();
 	}
 
-	printf("GAME OVER\n");
+	// clrtoeol();
+	refresh();
+	endwin();
+
+	printf("Game Over!\n");
 
 	return 0;
 }
 
-void add_piece(Snake* snake, int altura, int largura)
+void movement_snake(Snake* snake)
 {
+	int delta_y=0, delta_x=0;
 
-	if(snake->size == 0)
+	switch(snake->direction)
 	{
-		// head and tail
-		snake->Pieces=(Snake_Piece*)malloc(2*sizeof(Snake_Piece));
+		case 'D':
+			snake->Body[0].body = 'V';
+			delta_y = +1;
+		break;
 
-		
-		// head
-		snake->Pieces[0].pos_y = largura/2;
+		case 'U':
+			snake->Body[0].body = '^';
+			delta_y = -1;
+			break;
 
-		snake->Pieces[0].pos_x = altura/2;
-		snake->Pieces[0].piece = '>';
+		case 'R':
+			snake->Body[0].body = '>';
+			delta_x = 1;
+			break;
 
-		snake->Pieces[1].pos_y = (largura/2) - 1;
-		snake->Pieces[1].pos_x = altura/2;
-		snake->Pieces[1].piece = '-';
-
-		snake->size = 2;
+		case 'L':
+			snake->Body[0].body = '<';
+			delta_x = -1;
+			break;
 	}
 
-	else
-	{	
-		// adding new piece to Pieces array
-		snake->Pieces=realloc(snake->Pieces, snake->size+1);
-		snake->Pieces[snake->size+1] = snake->Pieces[snake->size];
-		snake->size++;
-	}
-
-}
-
-int playing(char** tabuleiro,Snake* snake, int altura, int largura)
-{
-	if(eat_itself(snake))
-		return 0;
-
-	// checking if snake head is on the board
-	if( snake->Pieces[0].pos_x < 0 		  || 
-		snake->Pieces[0].pos_y < 0 		  ||
-		snake->Pieces[0].pos_x >= largura || 
-		snake->Pieces[0].pos_y >= altura )
-			return 0;
-
-	return 1;
-}
-
-int eat_itself(Snake* snake)
-{
+	snake->Body[0].pos_x += delta_x;
+	snake->Body[0].pos_y += delta_y;
+	
 	for(int i=1; i<snake->size; i++)
 	{
-		if( snake->Pieces[0].pos_x == snake->Pieces[i].pos_x && 
-			snake->Pieces[0].pos_y == snake->Pieces[i].pos_y
-		  )
-		{
-			return 1;
-		}
+		snake->Body[i].pos_x = snake->Body[i-1].pos_x - delta_x;
+		snake->Body[i].pos_y = snake->Body[i-1].pos_y - delta_y;
+		snake->Body[i].body = snake->Body[i-1].body;
 	}
 
-	return 0;
 }
 
-// moving all the snake pieces coordinates
-void move_snake(Snake* snake)
+bool isGameOver(Snake* snake)
 {
+	// eat itself
+	for(int i=1; i<snake->size; i++)
+	{
+		if( snake->Body[0].pos_x == snake->Body[i].pos_x && 
+			snake->Body[0].pos_y == snake->Body[i].pos_y)
+			return TRUE;
+	}
+
+	// out of table
+	if(snake->Body[0].pos_x < 0 ||  snake->Body[0].pos_y < 0)
+		return TRUE;
+
+	// out of table
+	if(snake->Body[0].pos_x >= WIDTH)
+		return TRUE;
+
+	// out of table
+	if(snake->Body[0].pos_y >= HEIGHT)
+		return TRUE;
+
+	return FALSE;
+}
+
+// print table
+void print_table(WINDOW* main_win, char** table, Snake* snake)
+{
+	box(main_win, 0, 0);
+
+	insert_snake(table, snake);
+
+	// print table
+	for(int y=0; y<HEIGHT; y++)
+		for(int x=0; x<WIDTH; x++)
+		{
+			mvwprintw(main_win, y, x, "%c", table[y][x]);
+		}
+
+	wrefresh(main_win);
+}
+
+void insert_snake(char** table, Snake* snake)
+{
+	for(int i=0; i<HEIGHT; i++)
+		for(int j=0; j<WIDTH; j++)
+			table[i][j] = '.';
+
+	// put snake on the table
 	for(int i=0; i<snake->size; i++)
 	{
-		// x should be horiz -> fix later
-		snake->Pieces[i].pos_x += snake->verti;
-		snake->Pieces[i].pos_y += snake->horiz;
+		table[snake->Body[i].pos_y][snake->Body[i].pos_x] = snake->Body[i].body;
 	}
 }
 
-void print_table(char** tabuleiro, Snake* snake,int altura, int largura)
+char** create_table(int height, int width, char aux)
 {
-	// clean the screen
-	for(int i=0; i<100; i++) puts("\n");
+	char** table = (char**) malloc(height * sizeof(char*));
 
-
-	// reset the table
-	for(int i=0; i<altura; i++)
-		for(int j=0; j<largura; j++)
-			tabuleiro[i][j] = '.';
-
-	// add the snake pieces on the table
-	for(int i=0; i<snake->size; i++)
+	for(int y=0; y<height; y++)
 	{
-		printf("i: %d Pos_x: %d\n", i, snake->Pieces[i].pos_x);
-		printf("i: %d Pos_y: %d\n", i, snake->Pieces[i].pos_y);
+		table[y] = (char*) malloc((width) * sizeof(char));
 
-		tabuleiro[snake->Pieces[i].pos_x][snake->Pieces[i].pos_y] = snake->Pieces[i].piece;
-	}
-
-	// print the table
-	for(int i=0; i<altura; i++)
-	{
-		for(int j=0; j<largura; j++)
+		for(int x=0; x<width; x++)
 		{
-			printf("%c ", tabuleiro[i][j]);
+			table[y][x] = aux;
 		}
-
-		printf("\n");
 	}
 
-	// wait 1 second
-	sleep(1);
+	return table;
 }
 
-char** generate(int altura, int largura)
+Snake create_snake(int height, int width)
 {
-	char** tmp;
+	Snake snake;
 
-	tmp = malloc(altura * sizeof(char*)); // y
+	snake.size = 0;
+	snake.direction = 'R';
 
-	for(int i=0; i<altura; i++)
+	snake.Body = (Snake_Body*) malloc(3*sizeof(Snake_Body));
+
+	for(int i=0; i<3; i++)
 	{
-		tmp[i] = malloc((largura+1)*sizeof(char)); // x
-		
-		for(int j=0; j<largura; j++)
-		{
-			tmp[i][j] = '.';
-		}
+		snake.Body[i].pos_x = (width/2) - i;
+		snake.Body[i].pos_y = (height/2);
+
+		if(i==0)
+			snake.Body[i].body = '>';
+
+		else
+			snake.Body[i].body = '>';
+
+		snake.size++;
 	}
 
-	return tmp;
+	return snake;
 }
